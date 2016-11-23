fd92ee73363cac76115f511e289c03c4aedc3a23
a_bladyko <a_bladyko@wargaming.net>
2016-11-23 18:15:19 +0300

    made something work

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
+import 'es6-shim'
+// import 'es6-collections'
+import 'es6-promise'
+import 'isomorphic-fetch'
+
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Store, createStore, Reducer, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
+import { Store, createStore, Reducer, combineReducers, } from 'redux'
+import { Provider, } from 'react-redux'
 
 import config from './reducers/config'
 import Counter from './components/Counter'
-import { loadConfig } from './actions/config'
+// import { loadConfig, } from './actions/config'
 
-import { initialState } from 'state'
+import { initialState, IState, } from 'state'
 
 
-function configureStore(): Store<{}> {
-    const reducer: Reducer<{}> = combineReducers({
+function configureStore(): Store<IState> {
+    const reducer: Reducer<IState> = combineReducers<IState>({
         config,
     })
-    const store: Store<{}> = createStore(reducer, initialState)
+    const store: Store<IState> = createStore<IState>(reducer, initialState)
 
     // if (module.hot) {
     //     module.hot.accept('./reducers', () => {
@@ -26,7 +31,7 @@ function configureStore(): Store<{}> {
     return store
 }
 
-const store: Store<{}> = configureStore()
+const store: Store<IState> = configureStore()
 
 
 class Main extends React.Component<{}, {}> {
@@ -42,7 +47,7 @@ class Main extends React.Component<{}, {}> {
 
 ReactDOM.render(<Main />, document.getElementById('app'))
 
-loadConfig(store.dispatch)
+// loadConfig(store.dispatch)
 
 export function sum(a: number, b: number): number {
     return a + b
