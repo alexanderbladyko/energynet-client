21c62e5c379bd693b2b1628960634302d190f94e
alexanderbladyko <sanechekz@mail.ru>
2017-01-14 01:04:31 +0300

    added logic to auction

--- src/components/Auction/Auction.tsx
+++ src/components/Auction/Auction.tsx
@@ -7,6 +7,8 @@ import {
     requestAuction,
     receiveAuction,
     requestAuctionBet,
+    requestAuctionFold,
+    selectStation,
 } from 'actions/auction'
 import * as socket from 'api/socket'
 import * as State from 'state'
@@ -19,10 +21,15 @@ interface IAuctionProps {
     requestAuction: typeof requestAuction
     receiveAuction: typeof receiveAuction
     requestAuctionBet: typeof requestAuctionBet
+    requestAuctionFold: typeof requestAuctionFold
+    selectStation: typeof selectStation
 }
 
 
 class Auction extends React.Component<IAuctionProps, {}> {
+    public refs: {
+        betRange: (HTMLInputElement)
+    }
     public componentWillMount(): void {
         this.props.requestAuction()
         socket.send('auction', {})
@@ -40,18 +47,66 @@ class Auction extends React.Component<IAuctionProps, {}> {
                 <div>{'Loading...'}</div>
             )
         }
+        const userId: number = this.props.userInfo.data.id
+        const yourTurn: boolean = (this.props.game.meta.turn === userId)
+        const user: State.IGamePlayer = this.props.game.data.find(player => player.id === userId)
+        const lastBet: number = this.props.game.meta.auction && this.props.game.meta.auction.lastBet
+
         return (
             <div>
+                {
+                    yourTurn
+                    && !lastBet
+                    && 'Choose station'
+                }
+                {
+                    yourTurn
+                    && lastBet
+                    && (
+                        <div>
+                            <input ref='betRange' type='range' min={lastBet} max={user.cash} />
+                            <button onClick={() => this.handleAuctionBet()}>{'Raise'}</button>
+                        </div>
+                    )
+                }
+                {
+                    yourTurn
+                    && user.stations.length !== 0
+                    && <div>
+                        'or'
+                        <button onClick={() => this.handleAuctionFold}>{'Fold'}</button>
+                    </div>
+                }
                 {
                     this.props.auction.data
                     && this.props.auction.data.map(station => {
                         return (
-                            <Station key={station.cost} station={station} />
+                            <Station
+                                key={station.cost}
+                                station={station}
+                                onClick={() => this.handleStationSelect(station)}
+                            />
                         )
                     })
                 }
             </div>
         )
+    }
+    private handleStationSelect(station: State.IAuctionStation): void {
+        if (this.props.game.meta.turn !== this.props.userInfo.data.id) {
+            return
+        }
+        if (this.props.auction.selectedStationId === station.cost) {
+            return
+        }
+
+        this.props.selectStation(station)
+    }
+    private handleAuctionBet(): void {
+        this.props.requestAuctionBet(parseInt(this.refs.betRange.value, 10))
+    }
+    private handleAuctionFold(): void {
+        this.props.requestAuctionFold()
     }
 }
 
@@ -67,5 +122,7 @@ export default connect(
         requestAuction,
         receiveAuction,
         requestAuctionBet,
+        requestAuctionFold,
+        selectStation,
     }
 )(Auction)
--- src/components/Auction/Station.tsx
+++ src/components/Auction/Station.tsx
@@ -4,20 +4,24 @@ import * as State from 'state'
 
 interface IStationProps {
     station: State.IAuctionStation
+    onClick: any
 }
 
 
 export default class Station extends React.Component<IStationProps, {}> {
     public render(): React.ReactElement<{}> {
         return (
-            <div style={{
-                width: 200,
-                height: 300,
-                borderWidth: 1,
-                borderColor: '#123123',
-                borderStyle: 'solid',
-                borderRadius: 3,
-            }}>
+            <div
+                onClick={this.props.onClick}
+                style={{
+                    width: 200,
+                    height: 300,
+                    borderWidth: 1,
+                    borderColor: '#123123',
+                    borderStyle: 'solid',
+                    borderRadius: 3,
+                }}
+            >
                 <p>{this.props.station.cost}</p>
                 <p>{this.props.station.capacity}</p>
                 <p>{this.props.station.efficiency}</p>
